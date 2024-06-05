e3e06087cb32ec4f8901d66791259cf0eb57499f
David Brossard <6230719+davidjbrossard@users.noreply.github.com>
2024-06-05 13:17:35 +0200

    Updated the root policy name

--- scripts/update.bat
+++ scripts/update.bat
@@ -3,4 +3,4 @@ del /Q ..\xacml
 REM Convert ALFA to XACML
 java -jar "../bin/alfac.jar" "../alfa" -o ../xacml
 REM Assemble Domain file
-..\bin\domtool-0.9.0\bin\domtool domain --identity=demo --main-policy-id=http://axiomatics.com/alfa/identifier/demo.allowAll --attributes=../config/attributes.yaml  -o ../domain_tutorial.yaml --xacml-specifications ../xacml
+..\bin\domtool-0.9.0\bin\domtool domain --identity=demo --main-policy-id=http://axiomatics.com/alfa/identifier/demo.myPolicy --attributes=../config/attributes.yaml  -o ../domain_tutorial.yaml --xacml-specifications ../xacml
